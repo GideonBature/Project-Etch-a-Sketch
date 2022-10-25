cbe904e97a67594fb9cd60b05e335d03cbef9d64
GideonBature <infoaboutgideon@gmail.com>
2022-10-25 04:10:17 +0100

    add custom grid function

--- index.js
+++ index.js
@@ -1,22 +1,75 @@
 const container = document.getElementById('container');
+const btn = document.querySelector('button');
 
-let colsNum = 16;
-let rowsNum = 16;
 
-let grid = document.createElement('div');
-grid.className = "grid";
+function createDefaultGrid() {
+    let colsNum = 16;
+    let rowsNum = 16;
 
-for (let c = 0; c < colsNum; c++) {
-    let cols = document.createElement('div');
-    cols.className = "column"
-
-    for (let r = 0; r < rowsNum; r++) {
-        let rows = document.createElement('div');
-        rows.className = "row"
-        cols.appendChild(rows);
+    let grid = document.createElement('div');
+    grid.className = "grid";
+    
+    for (let c = 0; c < colsNum; c++) {
+        let cols = document.createElement('div');
+        cols.className = "column"
+    
+        for (let r = 0; r < rowsNum; r++) {
+            let rows = document.createElement('div');
+            rows.className = "row"
+            cols.appendChild(rows);
+        }
+    
+        grid.appendChild(cols);
     }
+    
+    container.appendChild(grid);
 
-    grid.appendChild(cols);
+    drawGrid();
+}
+createDefaultGrid()
+
+
+function drawGrid () {
+    let draw = document.querySelectorAll(".row");
+    
+    for (let draws of draw) {
+        draws.addEventListener('mouseover', () => {
+                draws.style.backgroundColor = "black";
+        } )
+    }
 }
 
-container.appendChild(grid);
+
+    let btnClick = btn.addEventListener('click', ()=> {
+    
+        let removeGrid = document.querySelector('.grid');
+        removeGrid.remove();
+
+        createCustomGrid();
+    
+    })
+    
+    function createCustomGrid() {
+        let colsNum = parseInt(prompt("enter number of columns"));
+        let rowsNum = parseInt(prompt('enter number of rows'));
+        let grid = document.createElement('div');
+        grid.className = "grid";
+        
+        for (let c = 0; c < colsNum; c++) {
+            let cols = document.createElement('div');
+            cols.className = "column"
+        
+            for (let r = 0; r < rowsNum; r++) {
+                let rows = document.createElement('div');
+                rows.className = "row"
+                cols.appendChild(rows);
+            }
+        
+            grid.appendChild(cols);
+        }
+        
+        container.appendChild(grid);
+    
+        drawGrid();
+    
+    }
