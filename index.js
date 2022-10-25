35287bc46a222d65c11083538cab747f91d4bba4
GideonBature <infoaboutgideon@gmail.com>
2022-10-26 00:38:29 +0100

    add random colors and modified style

--- index.js
+++ index.js
@@ -29,12 +29,18 @@ function createDefaultGrid() {
 createDefaultGrid()
 
 
+function randomColor() {
+    let randColor = Math.floor(Math.random()*255 + 1);
+    return randColor;
+
+}
+
 function drawGrid () {
     let draw = document.querySelectorAll(".row");
     
     for (let draws of draw) {
         draws.addEventListener('mouseover', () => {
-                draws.style.backgroundColor = "black";
+                draws.style.backgroundColor = `rgb(${randomColor()}, ${randomColor()}, ${randomColor()} )`;
         } )
     }
 }
@@ -52,23 +58,28 @@ function drawGrid () {
     function createCustomGrid() {
         let colsNum = parseInt(prompt("enter number of columns"));
         let rowsNum = parseInt(prompt('enter number of rows'));
-        let grid = document.createElement('div');
-        grid.className = "grid";
-        
-        for (let c = 0; c < colsNum; c++) {
-            let cols = document.createElement('div');
-            cols.className = "column"
-        
-            for (let r = 0; r < rowsNum; r++) {
-                let rows = document.createElement('div');
-                rows.className = "row"
-                cols.appendChild(rows);
+        if(colsNum <= 100 || rowsNum <= 100) {
+
+            let grid = document.createElement('div');
+            grid.className = "grid";
+            
+            for (let c = 0; c < colsNum; c++) {
+                let cols = document.createElement('div');
+                cols.className = "column"
+            
+                for (let r = 0; r < rowsNum; r++) {
+                    let rows = document.createElement('div');
+                    rows.className = "row"
+                    cols.appendChild(rows);
+                }
+            
+                grid.appendChild(cols);
             }
-        
-            grid.appendChild(cols);
+            
+            container.appendChild(grid);
+        } else {
+            createDefaultGrid();
         }
-        
-        container.appendChild(grid);
     
         drawGrid();
     
